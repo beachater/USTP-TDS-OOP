fdd21d8d07f2193656db44123965dd57310faba3
babic <pycoderesort@gmail.com>
2024-06-14 12:47:58 +0800

    updated pixels and hp

--- src/com/wizardtdshooter/view/Window.java
+++ src/com/wizardtdshooter/view/Window.java
@@ -9,7 +9,7 @@ public class Window {
 	public static final int WIDTH = 1000;
 	public static final int HEIGHT = 563;
 	public static final String operatingSystem = System.getProperty("os.name");
-	public static int hp = 500000;
+	public static int hp = 200;
 	public static int ammo = 30;
 
 	public Window(int width, int height, String title, Game game) {
